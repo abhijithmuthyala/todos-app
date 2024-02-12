8aabe1b41abe88fe4cc0dc98c48ff94d9deb2906
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-02-12 22:01:39 +0530

    Init design system - colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <h1 className="mx-6 px-4 sm:p-8  md:text-wrap ">Hello world</h1>
-    </>
-  );
+  return <></>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: "var(--blue-400)",
+      neutral: {
+        100: "var(--neutral-100)",
+        200: "var(--neutral-200)",
+        300: "var(--neutral-300)",
+        400: "var(--neutral-400)",
+        500: "var(--neutral-500)",
+        600: "var(--neutral-600)",
+        700: "var(--neutral-700)",
+        800: "var(--neutral-800)",
+        900: "var(--neutral-900)",
       },
+      white: "var(--white)",
+    },
+    gradientColorStops: {
+      "header-start": "var(--bg-gradient-start-header)",
+      "highlight-start": "var(--bg-gradient-start-highlight)",
+      "header-end": "var(--bg-gradient-end-header)",
+      "highlight-end": "var(--bg-gradient-end-highlight)",
     },
   },
   plugins: [],
